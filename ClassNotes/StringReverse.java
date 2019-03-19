c35795e961612c9d720672258fc4fd4b8990d458
Geoff Schmit <gcschmit@gmail.com>
2019-03-19 13:23:50 -0500

    added a method to reverse a string
    recursive and iterative solutions

---
+++ ClassNotes/StringReverse.java
@@ -0,0 +1,42 @@
+
+/**
+ * Write a description of class StringReverse here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class StringReverse
+{
+    public static String reverse(String str)
+    {
+        // must have a terminating case
+        if(str.equals(""))
+        {
+            return str;
+        }
+        
+        // must make the problem simplier
+        String firstChar = str.substring(0, 1);
+        String restOfString = str.substring(1);
+        
+        // must recurse -
+        //  call this method recursively with a simplier problem
+        String restOfStringReversed = reverse(restOfString);
+        
+        // return the solution
+        String strReversed = restOfStringReversed + firstChar;
+        return strReversed;
+    }
+    
+    public static String reverseIter(String str)
+    {
+        String strReversed = "";
+        
+        for(int i = 0; i < str.length(); i++)
+        {
+            strReversed = str.substring(i, i + 1) + strReversed;
+        }
+        
+        return strReversed;
+    }
+}
